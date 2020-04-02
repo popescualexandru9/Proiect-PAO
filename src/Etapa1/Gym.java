a70d3e16767e67146378a4ee2ff00bf2976f9b75
popescualexandru9 <popscu_alexandru9@yahoo.com>
2020-04-02 17:54:12 +0300

    Update Gym.java

--- src/Etapa1/Gym.java
+++ src/Etapa1/Gym.java
@@ -1,8 +1,12 @@
 package Etapa1;
 
+import Etapa1.Customers_and_Memberships.Customer;
+import Etapa1.Employees.Cashier;
+import Etapa1.Employees.Trainer;
+
 import java.util.*;
 
-public class Gym {
+class Gym {
     private static final String owner = "Christian Guzman";
     private static final String name = "Alphalete";
     private static final String address = "814 SUMMER PARK DR. STAFFORD";
@@ -10,55 +14,54 @@ public class Gym {
     private static final String phoneNumber = "0731876657";
     private List<Cashier> cashiers;
     private List<Trainer> trainers ;
-    private Map<String,Customer> customers = new HashMap<String,Customer>();;
+    private Map<String, Customer> customers = new HashMap<>();
 
-    public Gym() {
-        this.cashiers = new ArrayList<Cashier>();
-        this.trainers = new ArrayList<Trainer>();
-
+    Gym() {
+        this.cashiers = new ArrayList<>();
+        this.trainers = new ArrayList<>();
     }
 
-    public static String getOwner() {
+    static String getOwner() {
         return owner;
     }
 
-    public static String getName() {
+    static String getName() {
         return name;
     }
 
-    public static String getAddress() {
+    static String getAddress() {
         return address;
     }
 
-    public static String getMail() {
+    static String getMail() {
         return mail;
     }
 
-    public static String getPhoneNumber() {
+    static String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public List<Cashier> getCashiers() {
+    List<Cashier> getCashiers() {
         return cashiers;
     }
 
-    public List<Trainer> getTrainers() {
+    List<Trainer> getTrainers() {
         return trainers;
     }
 
-    public Map<String, Customer> getCustomers() {
+    Map<String, Customer> getCustomers() {
         return customers;
     }
 
-    public void addCashier(Cashier cashier){
+    void addCashier(Cashier cashier){
         cashiers.add(cashier);
     }
 
-    public void addTrainer(Trainer trainer){
+    void addTrainer(Trainer trainer){
         trainers.add(trainer);
     }
 
-    public boolean addCustomer(String phoneNumber,Customer customer) {
+    boolean addCustomer(String phoneNumber,Customer customer) {
         if(customers.get(phoneNumber) != null){
             System.out.println("Customer is already in data base.");
             return false;}
@@ -67,7 +70,7 @@ public class Gym {
         return true;
     }
 
-    public void modifyCustomer(String phoneNumber,Customer customer){
+    void modifyCustomer(String phoneNumber,Customer customer){
         customers.replace(phoneNumber,customer);
     }
 
